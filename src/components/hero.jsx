923101e645794bb5b0b028edc23cd7bdfda417f2
Pondering-Apple <136867740+Pondering-Apple@users.noreply.github.com>
2024-05-04 18:06:11 +1000

    Update hero.jsx

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="bg-hero-image bg-100% flex w-full h-96 justify-center items-center">
-      <h1 className="text-4xl font-light text-colorlight">
+      <h1 className="pt-20 text-5xl font-light text-colorlight">
         Timeless, Fast, Classic
       </h1>
     </div>
